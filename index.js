8d92bc026938bb6f99a3f188b1edc5dfffa2058c
janvi876 <jahanvigorajiya02@gmail.com>
2025-02-26 18:48:13 +0530

    github added in

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
+require('dotenv').config()
 const express = require('express')
+
 const app = express()
+
 const port = 3000
+
+const githubData = {
+    "timeline_url": "https://github.com/timeline",
+    "user_url": "https://github.com/{user}",
+    "current_user_public_url": "https://github.com/ayushsnha",
+    "_links": {
+      "timeline": {
+        "href": "https://github.com/timeline",
+        "type": "application/atom+xml"
+      },
+      "user": {
+        "href": "https://github.com/{user}",
+        "type": "application/atom+xml"
+      },
+      "current_user_public": {
+        "href": "https://github.com/ayushsnha",
+        "type": "application/atom+xml"
+      }
+    }
+  }
+    
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -16,6 +40,9 @@ app.get('/twitter', (req, res) => {
         res.send('<h2>please register</h2>')
     })
 
-app.listen(port, () => {
+  app.get('/github', (req, res) => {
+    res.json(githubData)
+  })
+app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`)
 })
